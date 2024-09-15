6e3239f5644fb29a32f6da5b20e83d0563efc206
Mustafa <pourmhere@gmail.com>
2024-09-15 18:00:51 +0530

    d

--- src/components/WeatherC.js
+++ src/components/WeatherC.js
@@ -7,7 +7,7 @@ const WeatherC = ({ weather }) => {
 
   return (
     <div>
-      <p style={tempStyle}>Temperature: {temperature} </p>
+      <p style={tempStyle}>Temperature: {temperature}</p>
       <p>Conditions: {conditions}</p>
       <span style={tempStyle}>Temperature Color Indicator</span>
     </div>
